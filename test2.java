6e17184450068e48f396cc82a643a0e6ecab4a39
JAGATHRATCHAGAN-P <110395817+JAGATHRATCHAGAN-P@users.noreply.github.com>
2022-08-05 01:26:14 -0700

    Add files via upload

---
+++ test2.java
@@ -0,0 +1,12 @@
+class test2
+{
+  public static void main(String[]arg)
+  {
+    int value1=15;
+	float value2=5.5F;
+	boolean result=true;
+	System.out.println(value1);
+	System.out.println(value2);
+	System.out.println(result);
+  }
+}	
